b4b673bdb8c693b0360c29ee22719565a372136c
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-04 16:47:39 +0100

    Modify item.rb file

--- item.rb
+++ item.rb
@@ -1,23 +1,51 @@
-require 'date'
+# frozen_string_literal: true
 
+# Represents an item in the catalog.
 class Item
-  attr_accessor :publish_date, :archived
+  attr_accessor :title, :description, :published_date, :archived
 
-  def initialize(published_date)
-    @id = rand(1...1000)
-    @genre = genre
-    @author = Author
-    @source = source
-    @label = Label
-    @publish_date = date
+  def initialize(title, description, published_date)
+    @title = title
+    @description = description
+    @published_date = published_date
     @archived = false
   end
 
   def can_be_archived?
-
+    current_year = Time.now.year
+    (current_year - @published_date.year) > 10
   end
 
   def move_to_archive
-
+    if can_be_archived?
+      @archived = true
+      puts "Item '#{title}' has been archived."
+    else
+      puts "Item '#{title}' cannot be archived at this time."
+    end
   end
 end
+
+# require 'date'
+
+# class Item
+#   attr_accessor :publish_date, :archived
+
+#   def initialize(published_date)
+#     @id = rand(1...1000)
+#     @genre = genre
+#     @author = Author
+#     @source = source
+#     @label = Label
+#     @publish_date = date
+#     @archived = false
+#   end
+
+#   def can_be_archived?
+
+#   end
+
+#   def move_to_archive
+
+#   end
+# end
