73aaf26c83a33411000cdfd8b088eb3d6c75f898
ometman <revomet@gmail.com>
2023-12-05 09:14:55 +0000

    fix linters

--- item.rb
+++ item.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
 
 require 'date'
-  
-class Item
-    attr_accessor :published_date, :genre, :author, :source, :label
-  
-    def initialize(published_date)
-      @id = rand(1...1000)
-      @genre = genre
-      @author = author
-      @source = source
-      @label = label
-      @published_date = date
-      @archived = false
-    end
-  
-    private
 
-    def can_be_archived?
-        (Date.today - @published_date).to_i > 365 * 10
-    end
-    
-    def move_to_archive
-        @archived = true if can_be_archived?
-    end
+# class item
+class Item
+  attr_accessor :published_date, :genre, :author, :source, :label
+
+  def initialize(published_date)
+    @id = rand(1...1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @published_date = published_date
+    @archived = false
+  end
+
+  private
+
+  def can_be_archived?
+    (Date.today - @published_date).to_i > 365 * 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
-  
