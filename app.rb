b7b636741537ac8bc21db0b8d6d92fa237c11b4f
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-05 11:45:44 +0100

    Modify and add some files

---
+++ app.rb
@@ -0,0 +1,55 @@
+require_relative 'classes/book'
+require_relative 'classes/label'
+
+class App
+  attr_accessor :books, :labels
+
+  def initialize
+    @labels = []
+    @books = []
+  end
+
+  def add_label(title, color)
+    label = Label.new(title, color)
+    @labels << label
+  end
+
+  def list_all_labels
+    if @labels.empty?
+      puts 'No labels added'
+      puts ' '
+    else
+      puts 'List of all labels:'
+      @labels.each_with_index do |label, index|
+        puts "#{index + 1}. Label: #{label.title}, Color: #{label.color}"
+        puts ' '
+      end
+    end
+  end
+
+  def list_all_books
+    if @books.empty?
+      puts 'No books added'
+      puts ' '
+    else
+      @books.each_with_index do |book, index|
+        print "#{index + 1}-[Book], "
+        print "ID: #{book.id}, "
+        print "Publisher: #{book.publisher}, "
+        print "Publish Date: #{book.publish_date}, "
+        print "Cover State: #{book.cover_state}, "
+        puts "Archived: #{book.can_be_archived?}"
+        puts ' '
+      end
+    end
+  end
+
+  def add_book(publisher, cover_state, published_date)
+    book = Book.new(published_date, publisher, cover_state)
+    @books << book
+    puts ' '
+    puts 'The book is added successfully ✅'
+    puts '__-_-__-_-_-__--__--__--__'
+    puts ' '
+  end
+end
