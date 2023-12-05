0aad985d11df565488ee4bbabd350aa300fa6894
BHObserver <BHObserver0@gmail.com>
2023-12-06 01:11:46 +0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -30,6 +30,21 @@ class CatalogApp
       end
     end
   end
+
+  def perform_listing_action(choice)
+    listing_actions = %i[list_books list_album list_games list_genre list_labels list_authors]
+    @app.send(listing_actions[choice - 1])
+  end
+
+  def perform_addition_action(choice)
+    addition_actions = %i[list_movies list_source add_new_book add_new_album add_game add_new_movie]
+    @app.send(addition_actions[choice - 7])
+    @app.save_data if choice == 11
+  end
+
+  def exit_app
+    puts "\e[31mThank you for using this app! ♥️ \e[0m"
+  end
 end
 
 CatalogApp.new.start
