6a90927ec4e9af2d9dcdffbd0759021bbb05dea1
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-05 10:11:09 +0100

    Modify and create folder for main.rb item.rb

---
+++ classes/main.rb
@@ -0,0 +1,28 @@
+def main
+  puts 'Welcome to the Catalog of my things App!'
+  puts '<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>'
+  puts
+  loop do
+    puts 'Please select an option:'
+    puts '1. List all books'
+    puts '2. List all labels'
+    puts '3. Add a book'
+    puts '4. Exit'
+    options = gets.chomp.to_i
+    case options
+    when 1
+      puts 'List of all books'
+    when 2
+      puts 'List of all labels'
+    when 3
+      puts 'Add a book'
+    when 4
+      puts 'Exiting the program. Goodbye!'
+      break
+    else
+      puts 'Invalid option. Please try again.'
+    end
+  end
+end
+
+main
