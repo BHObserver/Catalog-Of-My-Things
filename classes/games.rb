54fb080af07e26278c40d404798a27895082498e
BHObserver <BHObserver0@gmail.com>
2023-12-05 16:23:48 +0600

    Update games.rb

--- classes/games.rb
+++ classes/games.rb
@@ -3,5 +3,11 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
+  def initialize(multiplayer: false, last_played_at: nil, **item_params)
+    super(**item_params)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
 
 end
