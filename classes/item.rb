93b32535f3d480ed6713bf6cf07a7a37bd6299a8
BHObserver <BHObserver0@gmail.com>
2023-12-05 15:51:27 +0600

    Create item.rb

---
+++ classes/item.rb
@@ -0,0 +1,32 @@
+
+require 'date'
+
+class Item
+    attr_accessor :published_date, :genre, :author, :source, :label
+    attr_reader :author
+
+    def initialize(published_date)
+      @id = rand(1...1000)
+      @genre = genre
+      @author = author
+      @source = source
+      @label = label
+      @published_date = date
+      @archived = false
+    end
+
+    def author=(author)
+      @author = author
+      author.items << self unless author.items.include?(self)
+    end
+
+    private
+
+    def can_be_archived?
+        (Date.today - @published_date).to_i > 365 * 10
+    end
+
+    def move_to_archive
+        @archived = true if can_be_archived?
+    end
+end
