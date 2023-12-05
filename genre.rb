f970464f4313a834ff731e7b25f5ec2263244dba
ometman <revomet@gmail.com>
2023-12-05 12:44:11 +0000

    Create genre class

---
+++ genre.rb
@@ -0,0 +1,26 @@
+require_relative 'item'
+
+class Genre < Item
+    attr_accessor :name, :albums
+    attr_reader :id
+
+    def initialize(name, published_date)
+        super(published_date)
+        @id = generate_id
+        @name = name
+        @albums = []
+    end
+
+    def add_item(item)
+        return if @album.include?(item)
+
+        @albums << item
+        item.add_genre(self)
+    end
+
+    private
+
+    def generate_id
+        SecureRandom.rand(1..1000)
+    end
+end
