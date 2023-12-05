9fab2ceba50e9992b22209d605b77fdc158dce23
BHObserver <BHObserver0@gmail.com>
2023-12-06 01:11:29 +0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -9,6 +9,27 @@ class CatalogApp
     puts 'Welcome to the Catalog of Things!'
     main_menu
   end
+
+  private
+
+  def main_menu
+    loop do
+      display_main_options
+      choice = gets.chomp.to_i
+
+      case choice
+      when 1..6
+        perform_listing_action(choice)
+      when 7..12
+        perform_addition_action(choice)
+      when 13
+        exit_app
+        break
+      else
+        puts 'Invalid option, please try again.'
+      end
+    end
+  end
 end
 
 CatalogApp.new.start
