89c51e291638efd1811508d8def3a5b2db15d995
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-06 14:40:51 +0100

    Modify book.rb and label.rb files

--- classes/book.rb
+++ classes/book.rb
@@ -1,11 +1,10 @@
 require_relative 'item'
 
-# book.rb
 class Book < Item
-  attr_accessor :publisher, :published_date, :cover_state
+  attr_accessor :publisher, :cover_state
 
-  def initialize(published_date, publisher, cover_state, archived: true)
-    super(published_date, archived: archived)
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
