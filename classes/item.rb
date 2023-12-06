65e5baeac8b7e6506545d1b6539a54fcf7100b30
ometman <revomet@gmail.com>
2023-12-06 00:15:38 +0000

    Move genre intoclasses directory and restore item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,31 +1,56 @@
+require_relative 'genre'
+require_relative 'author'
+require_relative 'label'
 require 'date'
+require 'securerandom'
 
-# Item Class
+# class item
 class Item
-  attr_accessor :published_date, :genre, :source, :label
+  attr_accessor :published_date, :genre, :author, :label, :collection
 
-  def initialize(_published_date)
-    @id = rand(1...1000)
+  def initialize(published_date)
+    @id = generate_id
     @genre = genre
     @author = author
-    @source = source
     @label = label
-    @published_date = date
+    @published_date = published_date
     @archived = false
+    @item_collection = []
   end
 
+  def add_label(label)
+    @item_collection << label
+    label.add_item(self)
+  end
+
+  def add_genre(genre)
+    @item_collection << genre
+    genre.add_item(self)
+  end
+
+  # back up of Ometman code
+  # def add_author(author)
+  #   @item_collection << author
+  #   author.add_item(self)
+  # end
+
+  # Burhan code
   def author=(author)
     @author = author
     author.items << self unless author.items.include?(self)
   end
 
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
   private
+
+  def generate_id
+    SecureRandom.rand(1..1000)
+  end
 
   def can_be_archived?
     (Date.today - @published_date).to_i > 365 * 10
-  end
-
-  def move_to_archive
-    @archived = true if can_be_archived?
   end
 end
