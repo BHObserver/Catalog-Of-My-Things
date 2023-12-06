b78a1aa38157bc98c689d3500992db39d0d93b55
BHObserver <BHObserver0@gmail.com>
2023-12-06 12:39:26 +0600

    Update author_module.rb

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -1,34 +1,47 @@
 module AuthorModule
   def list_authors
-    if @authors.empty?
+    if authors.empty?
       puts "\n\e[31mNo authors available!\e[0m\n"
-      puts
     else
       puts "\nList of Authors \n\n"
-      puts '---------------------------------------------------------'
-      puts "| ID \t\t| Name \t\t\t\t\t|"
-      puts '---------------------------------------------------------'
-      @authors.each_with_index do |author, _index|
-        puts "| #{author.id} \t\t| #{author.first_name} #{author.last_name} \t\t\t\t|"
-        puts '---------------------------------------------------------'
-      end
+      display_author_table
     end
   end
 
   def add_author
     puts "\nAdd author details:"
-    print 'First name: '
-    first_name = gets.chomp.to_s
-    first_name = 'Unknown' if first_name.empty?
-    print 'Last name: '
-    last_name = gets.chomp.to_s
-    last_name = 'Unknown' if last_name.empty?
+    first_name = get_input('First name')
+    last_name = get_input('Last name')
+
     author = Author.new(first_name: first_name, last_name: last_name)
-    @authors << author
+    authors << author
     puts "\e[32mAuthor added successfully!\e[0m"
   end
 
   def find_author(id)
-    @authors.find { |author| author.id == id }
+    authors.find { |author| author.id == id }
+  end
+
+  private
+
+  def authors
+    @authors ||= []
+  end
+
+  def display_author_table
+    puts '---------------------------------------------------------'
+    puts "| ID \t\t| Name \t\t\t\t\t|"
+    puts '---------------------------------------------------------'
+
+    authors.each do |author|
+      puts "| #{author.id} \t\t| #{author.full_name.ljust(30)}|"
+      puts '---------------------------------------------------------'
+    end
+  end
+
+  def get_input(prompt)
+    print "#{prompt}: "
+    input = gets.chomp.to_s
+    input.empty? ? 'Unknown' : input
   end
 end
