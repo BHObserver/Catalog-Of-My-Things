44e880111e8704ca786710c519411a1aea37905c
BHObserver <BHObserver0@gmail.com>
2023-12-06 19:23:58 +0600

    Update author_module.rb

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -1,3 +1,5 @@
+# AuthorModule Class
+
 module AuthorModule
   def list_authors
     if authors.empty?
@@ -37,11 +39,27 @@ module AuthorModule
       puts "| #{author.id} \t\t| #{author.full_name.ljust(30)}|"
       puts '---------------------------------------------------------'
     end
+    save_data('authors.json', authors)
   end
 
   def get_input(prompt)
     print "#{prompt}: "
     input = gets.chomp.to_s
     input.empty? ? 'Unknown' : input
+  end
+
+  def save_data(file_path, data)
+    full_path = File.join('data', file_path)
+    FileUtils.mkdir_p(File.dirname(full_path)) unless File.directory?(File.dirname(full_path))
+    File.write(full_path, JSON.pretty_generate(data.map(&:to_hash)))
+  end
+
+  def load_data(file_path, entity_class)
+    full_path = File.join('data', file_path)
+    return [] unless File.exist?(full_path)
+
+    JSON.parse(File.read(full_path)).map do |data|
+      entity_class.new(*data.values_at(*entity_class.new(nil, nil).to_hash.keys))
+    end
   end
 end
