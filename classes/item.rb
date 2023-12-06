5f3e8d81336bd7795fdb1bcd1b7e365ce01d6c7f
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-06 14:41:22 +0100

    Add item.rb file

---
+++ classes/item.rb
@@ -0,0 +1,26 @@
+require 'date'
+
+class Item
+  attr_accessor :labels, :publish_date, :archived
+  attr_reader :id
+
+  def initialize(publish_date, archived: false)
+    @id = Random.rand(1..1000)
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+  end
+
+  def label=(label)
+    @labels << label unless @labels.include?(label)
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
+
+  def can_be_archived?
+    Date.today.year - @publish_date.year > 10
+  end
+end
