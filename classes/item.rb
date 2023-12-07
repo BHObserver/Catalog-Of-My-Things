9374aba9896a9c0226e4dc607f98892bcd82376c
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-07 09:44:36 +0100

    Fix linters error

--- classes/item.rb
+++ classes/item.rb
@@ -8,60 +8,60 @@ class Item
     @id = Random.rand(1..1000)
     @publish_date = Date.parse(publish_date)
 
-require 'securerandom'
+# require 'securerandom'
 
-require 'date'
-require 'securerandom'
+# require 'date'
+# require 'securerandom'
 
-class Item
-  attr_accessor :publish_date
-  attr_reader :id, :archived, :label, :author
+# class Item
+#   attr_accessor :publish_date
+#   attr_reader :id, :archived, :label, :author
 
-  def initialize(publish_date: nil, archived: false)
-    @id = SecureRandom.uuid
-    @publish_date = publish_date
-    @archived = archived
-  end
+#   def initialize(publish_date: nil, archived: false)
+#     @id = SecureRandom.uuid
+#     @publish_date = publish_date
+#     @archived = archived
+#   end
 
-  def label=(label)
-    @labels << label unless @labels.include?(label)
-  end
+#   def label=(label)
+#     @labels << label unless @labels.include?(label)
+#   end
 
-  def move_to_archive
-    return unless can_be_archived?
+#   def move_to_archive
+#     return unless can_be_archived?
 
-    @archived = true
-  end
+#     @archived = true
+#   end
 
-  def can_be_archived?
-    Date.today.year - @publish_date.year > 10
-    @label = label
-    label.add_item(self)
+#   def can_be_archived?
+#     Date.today.year - @publish_date.year > 10
+#     @label = label
+#     label.add_item(self)
 
-  end
+#   end
 
   def label=(label)
     @label = label
     label.items << self unless label.items.include?(self)
   end
 
-  def genre=(item)
-    @genre = item
-    genre.items << self unless genre.items.include?(self)
-  end
+  # def genre=(item)
+  #   @genre = item
+  #   genre.items << self unless genre.items.include?(self)
+  # end
 
-  def author=(author)
-    @author = author
-    author.add_item(self) unless author.items.include?(self)
-  end
-
-  def move_to_archive
-    @archived = true if can_be_archived?
-  end
+  # def author=(author)
+  #   @author = author
+  #   author.add_item(self) unless author.items.include?(self)
+  # end
 
   def move_to_archive
     @archived = true if can_be_archived?
   end
+
+  # def move_to_archive
+  #   @archived = true if can_be_archived?
+  # end
 
   private
 
