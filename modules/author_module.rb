50bee585418017342d749e5c9ef27cf5fd41608c
BHObserver <BHObserver0@gmail.com>
2023-12-07 17:46:31 +0600

    Update author_module.rb

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -1,5 +1,8 @@
 # AuthorModule Class
 
+require 'fileutils'
+require_relative '../data_manager'
+
 module AuthorModule
   def list_authors
     if authors.empty?
@@ -16,6 +19,7 @@ module AuthorModule
     last_name = get_input('Last name')
 
     author = Author.new(first_name: first_name, last_name: last_name)
+    DataManager.save_author(author)
     authors << author
     puts "\e[32mAuthor added successfully!\e[0m"
   end
@@ -36,30 +40,14 @@ module AuthorModule
     puts '---------------------------------------------------------'
 
     authors.each do |author|
-      puts "| #{author.id} \t\t| #{author.full_name.ljust(30)}|"
+      puts "| #{author.id} \t\t| #{author.first_name} #{author.last_name}"
       puts '---------------------------------------------------------'
     end
-    save_data('authors.json', authors)
   end
 
   def get_input(prompt)
     print "#{prompt}: "
     input = gets.chomp.to_s
     input.empty? ? 'Unknown' : input
-  end
-
-  def save_data(file_path, data)
-    full_path = File.join('data', file_path)
-    FileUtils.mkdir_p(File.dirname(full_path)) unless File.directory?(File.dirname(full_path))
-    File.write(full_path, JSON.pretty_generate(data.map(&:to_hash)))
-  end
-
-  def load_data(file_path, entity_class)
-    full_path = File.join('data', file_path)
-    return [] unless File.exist?(full_path)
-
-    JSON.parse(File.read(full_path)).map do |data|
-      entity_class.new(*data.values_at(*entity_class.new(nil, nil).to_hash.keys))
-    end
   end
 end
