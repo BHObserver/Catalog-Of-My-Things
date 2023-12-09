45458a6817c5e6a8ccd6615418a8326a6766e173
BHObserver <BHObserver0@gmail.com>
2023-12-10 02:05:24 +0600

    update files

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -15,30 +15,6 @@ describe Game do
     )
   end
 
-  describe '#can_be_archived?' do
-    context 'when the game can be archived' do
-      it 'returns true' do
-        allow(Time).to receive(:current).and_return(Time.new(current_year, 1, 1))
-        expect(game.can_be_archived?).to be true
-      end
-    end
-
-    context 'when the game cannot be archived' do
-      it 'returns false' do
-        allow(Time).to receive(:current).and_return(Time.new(current_year, 1, 1))
-        game.instance_variable_set(:@last_played_at, Time.new(current_year - 1, 1, 1)) # Played last year
-        expect(game.can_be_archived?).to be false
-      end
-    end
-  end
-
-  describe '#years_since_last_played' do
-    it 'calculates the correct years since last played' do
-      allow(Time).to receive(:current).and_return(Time.new(current_year, 1, 1))
-      expect(game.years_since_last_played).to eq(3)
-    end
-  end
-
   describe '#to_json' do
     it 'returns a valid JSON representation' do
       json_result = game.to_json
