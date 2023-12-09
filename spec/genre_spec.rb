256650780eabb6265c91281668d9eb58ce2ce4c6
ometman <revomet@gmail.com>
2023-12-09 11:26:03 +0000

    Add code for testing genre class

---
+++ spec/genre_spec.rb
@@ -0,0 +1,38 @@
+require_relative './classes/genre'
+
+RSpec.describe Genre do
+  describe '#initialize' do
+    it 'creates a Genre object with provided name and published date' do
+      name = 'Test Genre'
+      published_date = '2023-01-15'
+
+      genre = Genre.new(name, published_date)
+
+      expect(genre.name).to eq(name)
+      expect(genre.published_date).to eq(published_date)
+      expect(genre.items).to be_empty
+    end
+  end
+
+  describe '#add_item' do
+    it 'adds an item to the genre' do
+      genre = Genre.new('Test Genre', '2023-01-15')
+      item = double('item')
+
+      genre.add_item(item)
+
+      expect(genre.items).to include(item)
+      expect(item.genre).to eq(genre)
+    end
+
+    it 'does not add the same item multiple times' do
+      genre = Genre.new('Test Genre', '2023-01-15')
+      item = double('item')
+
+      genre.add_item(item)
+      genre.add_item(item)
+
+      expect(genre.items.size).to eq(1)
+    end
+  end
+end
